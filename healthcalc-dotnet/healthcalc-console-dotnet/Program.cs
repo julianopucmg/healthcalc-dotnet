aa35675c8822886f1be3c08dd7836e5b12a16cbd
julianopucmg <1435615@sga.pucminas.br>
2023-10-28 19:39:56 -0300

    Criação do programa usando pacote nuget

--- healthcalc-dotnet/healthcalc-console-dotnet/Program.cs
+++ healthcalc-dotnet/healthcalc-console-dotnet/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using healthcalc_pack_dotnet;
+using healthcalc_pack_dotnet.Interfaces;
+
+IIMC objeto = new IMC();
+
+var imc = objeto.CalcularIMC(73.00, 1.72);
+var resultado = objeto.RetornarClassificacaoIMC(imc);
+
+Console.WriteLine("Seu resultado é: " + resultado);

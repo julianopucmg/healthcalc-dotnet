33da1c2c22ecfdbbe72fe6e670ff68c29c26b572
julianopucmg <1435615@sga.pucminas.br>
2023-10-29 17:59:59 -0300

    Incluído calculadora de marcronutrientes.

---
+++ healthcalc-dotnet/healthcalc-test-dotnet/Pack/MacronutrientesTest.cs
@@ -0,0 +1,31 @@
+﻿using healthcalc_pack_dotnet;
+using healthcalc_pack_dotnet.Enums;
+using healthcalc_pack_dotnet.Interfaces;
+using healthcalc_pack_dotnet.Models;
+
+namespace healthcalc_test_dotnet.Pack
+{
+    public class MacronutrientesTests
+    {
+        [Fact]
+        public void CalculaResultadoMacro()
+        {
+            //Arrange
+            IMacroNutrientes macros = new MacroNutrientes();
+            double Peso = 72;
+            var ObjetivoFisico = ObjetivoFisicoEnum.Keep;
+            var MacrosEsperados = new ResultadoMacroNutrientesModel()
+            {
+                Proteinas = 144,
+                Gorduras = 72,
+                Carboidratos = 360
+            };
+
+            //Act
+            var resultadoMacros = macros.CalcularMacronutrientes(Peso, ObjetivoFisico);
+
+            //Asserts
+            Assert.Equivalent(MacrosEsperados, resultadoMacros);
+        }
+    }
+}
